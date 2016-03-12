bb8f4b04383c1603bd0e7b0a46d82e58348eda14
MaYanTing <838502220@qq.com>
2016-03-12 15:38:13 -0800

    MYT
    add

---
+++ TextDemo/TextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextDemo
+//
+//  Created by MaYanTing on 16/3/9.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextDemo/TextDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TextDemo
+//
+//  Created by MaYanTing on 16/3/9.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
